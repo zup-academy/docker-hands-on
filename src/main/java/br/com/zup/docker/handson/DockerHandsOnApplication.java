04dcec10b9b854778c95fd71ebc58aff5f54fe06
Claudio E. de Oliveira <claudio.eduardo@zup.com.br>
2020-07-21 07:57:34 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/docker/handson/DockerHandsOnApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.docker.handson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerHandsOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerHandsOnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/docker/handson/DockerHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.docker.handson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerHandsOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
